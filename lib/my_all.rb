2d8be955e6ee6063c83e7355400e6dbdc5683aa5
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-23 20:30:39 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,6 +4,7 @@ def my_all?(collection)
     i = 0 
     block_return_values = []
   while i < collection.length 
+    block_return_values << yield(collection[i])
     yield(collection[i])
     i = i + 1
   end 
